47e4678714d092eabbe8b5213f9cd4d124f29e9e
Mike Ofori <102876495+michaelofori@users.noreply.github.com>
2023-01-26 11:22:33 +0000

    push

--- keepghanaclean/lib/main.dart
+++ keepghanaclean/lib/main.dart
@@ -4,6 +4,8 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:keepghanaclean/pages/aboutuspage.dart';
+import 'package:keepghanaclean/pages/instructions_page.dart';
 import 'package:keepghanaclean/pages/onboarding.dart';
 import 'package:keepghanaclean/pages/profile.dart';
 import 'package:splash_screen_view/SplashScreenView.dart';
@@ -35,7 +37,7 @@ class Splash extends StatelessWidget {
         color: Color.fromARGB(255, 42, 23, 207),
         fontSize: 30.0,
       ),
-      backgroundColor: Color.fromARGB(255, 14, 13, 13),
+      backgroundColor: Color.fromARGB(255, 255, 255, 255),
     );
 
     return MaterialApp(
@@ -140,39 +142,8 @@ class _MyStatefulWidgetState extends State<MyStatefulWidget> {
           ],
           automaticallyImplyLeading: false,
         ),
-        //second app
-        // AppBar(
-        //   title: const Text(
-        //     "The Middlemen Garage",
-        //     style: TextStyle(color: Colors.black),
-        //   ),
-        //   centerTitle: true,
-        //   elevation: 0.1,
-        //   backgroundColor: Color.fromARGB(255, 182, 58, 58),
-        //   leading: IconButton(
-        //     icon: const Icon(
-        //       Icons.menu,
-        //       color: Colors.black,
-        //     ),
-        //     onPressed: () {
-        //       _key.currentState?.openDrawer();
-        //     },
-        //   ),
-        //   actions: <Widget>[
-        //     IconButton(
-        //         onPressed: () {},
-        //         icon: const Icon(
-        //           Icons.search,
-        //           color: Colors.black,
-        //         )),
-        //   ],
-        //   automaticallyImplyLeading: false,
-        // ),
-        //profile app
         AppBar(
           title: const Text("Profile"),
-          // automaticallyImplyLeading: false,
-          // centerTitle: true,\
           backgroundColor: Color.fromARGB(255, 46, 90, 184),
           actions: <Widget>[
             IconButton(
@@ -192,30 +163,30 @@ class _MyStatefulWidgetState extends State<MyStatefulWidget> {
       ][currentIndex],
       drawer: _myDrawer(),
       body: pages[currentIndex],
-      bottomNavigationBar: BottomNavigationBar(
-        onTap: onTap,
-        currentIndex: currentIndex,
-        type: BottomNavigationBarType.fixed,
-        iconSize: 30,
-        selectedFontSize: 0,
-        unselectedFontSize: 0,
-        elevation: 0,
-        showUnselectedLabels: false,
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home_filled),
-            label: 'Home',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.local_activity),
-            label: 'Activity',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.person_sharp),
-            label: 'Profile',
-          ),
-        ],
-      ),
+      // bottomNavigationBar: BottomNavigationBar(
+      //   onTap: onTap,
+      //   currentIndex: currentIndex,
+      //   type: BottomNavigationBarType.fixed,
+      //   iconSize: 30,
+      //   selectedFontSize: 0,
+      //   unselectedFontSize: 0,
+      //   elevation: 0,
+      //   showUnselectedLabels: false,
+      //   items: const <BottomNavigationBarItem>[
+      //     BottomNavigationBarItem(
+      //       icon: Icon(Icons.home_filled),
+      //       label: 'Home',
+      //     ),
+      //     BottomNavigationBarItem(
+      //       icon: Icon(Icons.local_activity),
+      //       label: 'Activity',
+      //     ),
+      //     BottomNavigationBarItem(
+      //       icon: Icon(Icons.person_sharp),
+      //       label: 'Profile',
+      //     ),
+      //   ],
+      // ),
     );
   }
 
@@ -258,24 +229,28 @@ class _MyStatefulWidgetState extends State<MyStatefulWidget> {
             leading: const Icon(Icons.home),
             title: const Text("Home"),
           ),
-          // ListTile(
-          //   selected: cartColor,
-          //   onTap: () {
-          //     Navigator.pop(context);
-          //     setState(
-          //       () {
-          //         currentIndex = 1;
-          //         cartColor = true;
-          //         homeColor = false;
-          //         aboutColor = false;
-          //         contactUsColor = false;
-          //         logOut = false;
-          //       },
-          //     );
-          //   },
-          //   leading: const Icon(Icons.message_outlined),
-          //   title: const Text("Messages"),
-          // ),
+          ListTile(
+            selected: cartColor,
+            onTap: () {
+              Navigator.pop(context);
+              setState(
+                () {
+                  currentIndex = 1;
+                  cartColor = true;
+                  homeColor = false;
+                  aboutColor = false;
+                  contactUsColor = false;
+                  logOut = false;
+                },
+              );
+              // Navigator.push(
+              //   context,
+              //   MaterialPageRoute(builder: (context) => const MyStatefulWidget()),
+              // );
+            },
+            leading: const Icon(Icons.topic),
+            title: const Text("My Coupons"),
+          ),
           ListTile(
             selected: aboutColor,
             onTap: () {
@@ -294,29 +269,109 @@ class _MyStatefulWidgetState extends State<MyStatefulWidget> {
               //   MaterialPageRoute(builder: (context) => uploadCars()),
               // );
             },
-            leading: const Icon(Icons.upload_rounded),
-            title: const Text("Upload Feed"),
+            leading: const Icon(Icons.location_city_sharp),
+            title: const Text("Recycle Sites"),
           ),
-          // ListTile(
-          //   selected: contactUsColor,
-          //   onTap: () {
-          //     setState(
-          //       () {
-          //         contactUsColor = true;
-          //         homeColor = false;
-          //         cartColor = false;
-          //         aboutColor = false;
-          //         logOut = false;
-          //       },
-          //     );
-          //     Navigator.push(
-          //       context,
-          //       MaterialPageRoute(builder: (context) => Profile()),
-          //     );
-          //   },
-          //   leading: const Icon(Icons.contact_page_outlined),
-          //   title: const Text("Profile"),
-          // ),
+          ListTile(
+            selected: contactUsColor,
+            onTap: () {
+              setState(
+                () {
+                  contactUsColor = true;
+                  homeColor = false;
+                  cartColor = false;
+                  aboutColor = false;
+                  logOut = false;
+                },
+              );
+              // Navigator.push(
+              //   context,
+              //   MaterialPageRoute(builder: (context) => Profile()),
+              // );
+            },
+            leading: const Icon(Icons.leaderboard_sharp),
+            title: const Text("League Table"),
+          ),
+           ListTile(
+            selected: contactUsColor,
+            onTap: () {
+              setState(
+                () {
+                  contactUsColor = true;
+                  homeColor = false;
+                  cartColor = false;
+                  aboutColor = false;
+                  logOut = false;
+                },
+              );
+              // Navigator.push(
+              //   context,
+              //   MaterialPageRoute(builder: (context) => Profile()),
+              // );
+            },
+            leading: const Icon(Icons.redeem_outlined),
+            title: const Text("Redeem Products"),
+          ),
+           ListTile(
+            selected: contactUsColor,
+            onTap: () {
+              setState(
+                () {
+                  contactUsColor = true;
+                  homeColor = false;
+                  cartColor = false;
+                  aboutColor = false;
+                  logOut = false;
+                },
+              );
+              // Navigator.push(
+              //   context,
+              //   MaterialPageRoute(builder: (context) => Profile()),
+              // );
+            },
+            leading: const Icon(Icons.support_agent_rounded),
+            title: const Text("Support Center"),
+          ),
+           ListTile(
+            selected: contactUsColor,
+            onTap: () {
+              setState(
+                () {
+                  contactUsColor = true;
+                  homeColor = false;
+                  cartColor = false;
+                  aboutColor = false;
+                  logOut = false;
+                },
+              );
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => InstructionPage()),
+              );
+            },
+            leading: const Icon(Icons.integration_instructions_outlined),
+            title: const Text("Instructions Page"),
+          ),
+           ListTile(
+            selected: contactUsColor,
+            onTap: () {
+              setState(
+                () {
+                  contactUsColor = true;
+                  homeColor = false;
+                  cartColor = false;
+                  aboutColor = false;
+                  logOut = false;
+                },
+              );
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => AboutUsPage()),
+              );
+            },
+            leading: const Icon(Icons.contact_support),
+            title: const Text("About Us"),
+          ),
           ListTile(
             leading: const Icon(Icons.logout),
             title: const Text("Log out"),
